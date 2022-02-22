d2f89a460ee26fc377443d956bf58fc242f72d20
Samuel Gonzalez <samglz10@gmail.com>
2022-02-21 20:03:10 -0500

    first Edit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your Code Here+
 let firstAnswer  = window.prompt('Do you head left or right?')
 if(firstAnswer === 'left'){
-    let secondAnswer = window.prompt(`You come across a stray cat. 
+    let secondAnswer = window.prompt(`You come across a stray cat.. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
     if (secondAnswer === 'follow it') {
